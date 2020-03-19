017bed5a58d333f0ff6af3a1e306e87efee8345f
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-03-19 09:54:55 +0500

    test server route add

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
+router.get('/test', (req, res, next) => {
+	return res.status(200).json({
+		connection: 'ok',
+		message: 'test server'
+	})
+});
 router.use('/api', require('./api'));
 
 module.exports = router;
