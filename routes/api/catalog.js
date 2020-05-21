f985a70cc32949d43915912970446df3489f4233
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-05-21 12:42:12 +0500

    move catalog file

--- routes/api/catalog.js
+++ routes/api/catalog.js
@@ -1,5 +1,5 @@
 const auth = require('../../handlers/auth');
-const { catalog, updateCatalog } = require('../../handlers/catalog');
+const { catalog, updateCatalog } = require('../../controllers/catalog');
 const router = require('express').Router();
 
 router.get('/', auth.required, catalog);
