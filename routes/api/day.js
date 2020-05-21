13091ce60aed9ccbea8f1ea6cf6478dff693ea84
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-05-21 11:23:04 +0500

    delete useless methods

--- routes/api/day.js
+++ routes/api/day.js
@@ -1,18 +1,14 @@
 const router = require('express').Router();
 const fns = require('../../handlers/fns');
 const {
-  getDay,
   getDays,
   postDay,
-  updateDay,
   postDayByReceipt,
 } = require('../../controllers/day');
 const auth = require("../../handlers/auth");
 
 router.get('/', auth.required, getDays);
-router.get('/:year/:month/:day', auth.required, getDay);
 router.post('/', auth.required, postDay);
-router.put('/:year/:month/:day', auth.required, updateDay);
 router.post('/receipt', auth.optional, fns.checkAndReceive, postDayByReceipt);
 
 module.exports = router;
