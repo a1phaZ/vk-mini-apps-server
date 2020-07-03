40ba6f4d6dee416021cb9d6cdef0036c860b41f6
Artemiy Zebzeev <artemiy.zebzeev@gmail.com>
2020-07-03 11:02:34 +0500

    add delete route

--- routes/api/day.js
+++ routes/api/day.js
@@ -5,12 +5,14 @@ const {
   postDay,
   postDayByReceipt,
   editDayItem,
+  deleteDayItem,
 } = require('../../controllers/day');
 const auth = require("../../handlers/auth");
 
 router.get('/', auth.required, getDays);
 router.post('/', auth.required, postDay);
 router.put('/', auth.required, editDayItem);
+router.delete('/', auth.required, deleteDayItem);
 router.post('/receipt', auth.required, fns.checkAndReceive, postDayByReceipt);
 
 module.exports = router;
